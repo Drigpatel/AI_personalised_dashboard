2d2101b180ca35d887804d92f45e62cf89da347f
Drigpatel <pdirg066@gmail.com>
2025-11-17 18:00:50 -0500

    AI_personalised_dashboard-main

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const customerRoutes = require('./Routes/customer');
+const cookieParser = require('cookie-parser');
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://ai-dashboard-frontend-ten.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+}));
+app.use(cookieParser());
+const authRoutes = require('./Routes/auth');
+const adminRoutes = require('./Routes/admin');
+app.use('/api/auth', authRoutes);
+app.use('/api/admin', adminRoutes);
+app.use('/api', customerRoutes);
+
+const userRoutes = require('./Routes/user');
+app.use('/api', userRoutes);
+const PORT = process.env.PORT || 8181;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
